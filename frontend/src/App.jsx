8e5d267add790e8a95602420a186491d702a6229
Vikram Choudhary <vikrammc789@gmail.com>
2025-10-10 15:55:48 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { createContext, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import { Outlet, Route, Routes } from 'react-router-dom'
